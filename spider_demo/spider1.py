85dfb1be91d8fe7da6a913056713aef73296c898
home <2463072824@qq.com>
2019-03-08 00:07:21 +0800

    sku_lib

---
+++ spider_demo/spider1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@time    :2018/10/3 17:20
+#!@Author :SINGLE
+#!@File   :BaseApp.py
+from urllib.robotparser import RobotFileParser
+import re,json,requests,time
+from requests.exceptions import RequestException
+from pyquery import PyQuery as pq
+import  csv
+def is_can_catch():
+    rp=RobotFileParser('http://www.maoyan.com/robots.txt')
+    rp.read()
+    print(rp.can_fetch('*','https://maoyan.com/board/4?offset=0'))
+t='''
+   <dd>
+                        <i class="board-index board-index-1">1</i>
+    <a href="/films/1203" title="霸王别姬" class="image-link" data-act="boarditem-click" data-val="{movieId:1203}">
+      <img src="//s0.meituan.net/bs/?f=myfe/mywww:/image/loading_2.e3d934bf.png" alt="" class="poster-default"/>
+      <img data-src="https://p1.meituan.net/movie/20803f59291c47e1e116c11963ce019e68711.jpg@160w_220h_1e_1c" alt="霸王别姬" class="board-img"/>
+    </a>
+    <div class="board-item-main">
+      <div class="board-item-content">
+              <div class="movie-item-info">
+        <p class="name"><a href="/films/1203" title="霸王别姬" data-act="boarditem-click" data-val="{movieId:1203}">霸王别姬</a></p>
+        <p class="star">
+                主演：张国荣,张丰毅,巩俐
+        </p>
+<p class="releasetime">上映时间：1993-01-01</p>    </div>
+    <div class="movie-item-number score-num">
+<p class="score"><i class="integer">9.</i><i class="fraction">5</i></p>        
+    </div>
+
+      </div>
+    </div>
+
+                </dd>
+    '''
+def parse_page(pageIndex):
+    try:
+        response = requests.get('https://maoyan.com/board/4', data='{"offset":' + str(pageIndex) + '}')
+        if response.status_code==200:
+            doc=pq(response.text)
+            items= doc('dd').items()
+            for item in items:
+                index=item.find('.board-index').text()
+                img = item.find('.board-img').attr('data-src')
+                title = item.find('.name a ').text()
+                actor = item.find('.star').text()[3:]
+                time = item.find('.releasetime').text().lstrip()[5:]
+                score=item.find('.integer').text()+item.find('.fraction').text()
+
+                print(index,img,title,actor,time,score)
+                with open('data.csv', 'a', encoding='utf-8') as csvfile:
+                    fieldnames = ['index', 'img', 'title', 'actor','time','score']
+                    writer = csv.writer(csvfile)
+                    writer.writerow([index,img,title,actor,time,score])
+    except RequestException:
+        return None
+
+if  __name__=='__main__':
+    # if  not is_can_catch():
+    #     return
+    page = parse_page(0)
