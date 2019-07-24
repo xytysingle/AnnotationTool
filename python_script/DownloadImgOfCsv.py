f3d5523a7788feae75c937873df54e3f49b73cd5
xyty <2463072824@qq.com>
2019-07-24 21:48:05 +0800

    sku_lib

--- python_script/DownloadImgOfCsv.py
+++ python_script/DownloadImgOfCsv.py
@@ -2,11 +2,13 @@
 #!-*-coding:utf-8 -*-
 
 import requests
-import os
+import time
 import json
+import os
 import pandas as pds
 def main():
-    data = pds.read_csv(open('./res/冰露堆头24.csv'),header=None)
+    data_src='/data/tanx/res/data_src/冰露堆头24.csv'
+    data = pds.read_csv(open(data_src),header=None)
     pds.set_option('display.max_columns', 1000)
     pds.set_option('display.width', 1000)
     pds.set_option('display.max_colwidth', 1000)
@@ -18,17 +20,24 @@ def main():
     # print(json.load(data.iloc[0][0])[0])
 
     id=0
-    for i in range(data.shape[0]):
-        #读取指定行列
-        img_urls = json.loads(data.iloc[i,0])
-        # print(type(img_urls))
-        # 遍历获取图片链接并下载到本地
-        for img_url in img_urls:
-            response = requests.get(img_url)
-            img = response.content
-            with open('./imgs/{0}.jpg'.format(id), 'wb') as file:
-                file.write(img)
-            id += 1
+    dir_name=os.path.join('/data/tanx/res/output/',os.path.split(data_src)[1].split('.')[0]+'_'+time.strftime("%Y%m%d%H%M%S",time.localtime(time.time())))
+    #print(dir_name)
+    os.mkdir(dir_name)
+    if os.path.exists(dir_name):
+        for i in range(data.shape[0]):
+            #读取指定行列
+            print(id,id == 15)
+            # if id > 5:
+            #     break
+            img_urls = json.loads(data.iloc[i,0])
+            # 遍历获取图片链接并下载到本地
+            for img_url in img_urls:
+                response = requests.get(img_url)
+                img = response.content
+                img_name=time.strftime("%Y%m%d%H%M%S",time.localtime(time.time()))
+                with open(os.path.join(dir_name,f'{img_name}.jpg'), 'wb') as file:
+                    file.write(img)
+                id += 1
     #
     print('conout %d' % id)
 if __name__ == '__main__':
